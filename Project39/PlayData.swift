dfb9d5affb8c0ee9fa74919b4a5114b02bcfb255
Diego Pérez <diegodevelops@gmail.com>
2023-10-18 10:54:59 -0400

    added another test

--- Project39/PlayData.swift
+++ Project39/PlayData.swift
@@ -9,6 +9,7 @@ import Foundation
 
 class PlayData {
     var allWords = [String]()
+    var wordCounts = [String:Int]()
     
     init() {
         if let path = Bundle.main.path(forResource: "plays", ofType: "txt") {
@@ -16,6 +17,12 @@ class PlayData {
                 allWords = plays.components(separatedBy: CharacterSet.alphanumerics.inverted)
                 
                 allWords = allWords.filter { $0 != "" }
+                
+                for word in allWords {
+                    wordCounts[word, default: 0] += 1
+                }
+                
+                allWords = Array(wordCounts.keys)
             }
         }
     }
