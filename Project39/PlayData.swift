ca9e7e6440268a7ebefcfcf2e1fbb43447420ef7
Diego Pérez <diegodevelops@gmail.com>
2023-10-18 11:40:48 -0400

    adding filtering and corresponding test

--- Project39/PlayData.swift
+++ Project39/PlayData.swift
@@ -10,6 +10,7 @@ import Foundation
 class PlayData {
     var allWords = [String]()
     var wordCounts: NSCountedSet!
+    private(set) var filteredWords = [String]()
     
     init() {
         if let path = Bundle.main.path(forResource: "plays", ofType: "txt") {
@@ -23,5 +24,17 @@ class PlayData {
                 allWords = sorted as! [String]
             }
         }
+    }
+    
+    func applyUserFilter(_ input: String) {
+        if let userNumber = Int(input) {
+            applyFilter { self.wordCounts.count(for: $0) >= userNumber }
+        } else {
+            applyFilter { $0.range(of: input, options: .caseInsensitive) != nil }
+        }
+    }
+    
+    func applyFilter(_ filter: (String) -> Bool) {
+        filteredWords = allWords.filter(filter)
     }
 }
